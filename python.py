1e05a3ebbd9fc8f17f1525d673b69b06c8ec7b5a
Peter Brown <peterbrown454@gmail.com>
2024-01-17 00:13:13 +0000

    dunno

---
+++ python.py
@@ -0,0 +1,9 @@
+color = "blue"
+primary = 'True'
+
+first_statement = "The color is blue"
+second_statement = 'Either the color is red or primary is True'
+
+# write your if statement here
+if color == blue:
+    print('first_statement')
